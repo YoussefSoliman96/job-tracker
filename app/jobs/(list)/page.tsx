6d95a9318ca69973c0fa38debb3d48ad121de32d
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-21 00:25:41 +0300

    Refactor: Organize import statements

--- app/jobs/(list)/page.tsx
+++ app/jobs/(list)/page.tsx
@@ -1,8 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import JobStatusBadge from "../../components/JobStatusBadge";
 import JobActions from "../JobActions";
-import Link from "@/app/components/Link";
+import { JobStatusBadge, Link } from "@/app/components";
 
 const JobsPage = async () => {
   const jobs = await prisma.job.findMany();
