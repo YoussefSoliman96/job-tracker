71924153122742d9ad85f473a8bdd4d16f63fcfb
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-20 19:39:54 +0300

    Handle form submission

--- app/jobs/new/page.tsx
+++ app/jobs/new/page.tsx
@@ -1,13 +1,31 @@
+"use client";
+
 import { Button, TextArea, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+interface JobForm {
+  title: string;
+  description: string;
+}
 
 const NewJobPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<JobForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <TextArea placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/jobs", data);
+        router.push("/jobs");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <TextArea placeholder="Description" {...register("description")} />
       <Button>Submit New Job</Button>
-    </div>
+    </form>
   );
 };
 
