d6d245a943a08d9ed9d1866ee6abdb509e9386e1
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-20 17:29:15 +0300

    Build an API for creating Jobs

---
+++ app/api/jobs/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createJobSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createJobSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(
+      { error: validation.error.errors },
+      { status: 400 }
+    );
+
+  const newJob = await prisma.job.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(newJob, { status: 201 });
+}
