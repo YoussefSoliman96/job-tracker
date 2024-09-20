fb4d3f272e07763d8f13e34a37d17847f0f10b26
youssef soliman <youssef.soliman6636@gmail.com>
2024-09-20 19:08:43 +0300

    Fix Font bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
-import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+
 import "./globals.css";
 import NavBar from "./NavBar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -28,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.variable}>
         <Theme accentColor="iris">
           <NavBar />
           <main className="p-5">{children}</main>
